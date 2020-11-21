1f60224c717fa8b0cc21e3334eea0331078d0382
batotnv <bartosznguyenvan@gmail.com>
2020-11-21 23:47:34 +0100

    init

---
+++ projekt_IO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projekt_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("siema, fajny projekcik");
+        }
+    }
+}
